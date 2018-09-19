709981d2a5fad53b031a271bf6a9211cc3d2d9cd
ZHANGXIANG <863413076@qq.com>
2018-09-19 18:11:50 +0800

    first commit

---
+++ src/main/java/com/zx/TestController.java
@@ -0,0 +1,27 @@
+package com.zx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ZX on 2018/9/19.
+ *
+ * @param
+ * @return
+ */
+
+@RestController
+@EnableAutoConfiguration
+public class TestController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestController.class, args);
+    }
+}
